fb809fb56033e35381595abe909f1d882ddff207
Anjan Thapa <anzzanthapa@gmail.com>
2025-12-11 09:04:09 +0545

    Updated code and fixed paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import CoinDetail from "./Pages/CoinDetail";
+import CoinDetail from "./pages/CoinDetail";
 import { Coin } from "./pages/Coin";
 
 function App() {
